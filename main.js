c3c26cef27bf44ab22c26fe7e3b5f983663fce03
Yefferson <yefferson073@gmail.com>
2023-12-11 08:47:09 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,38 @@
+let products = {
+    data:[{
+        productName: "Regular white T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+    },
+    {
+        productName: "Beige Short Skirt",
+        category: "Bottomwear",
+        price: "49",
+        image: "short-skirt.jpg",
+    },
+    {
+        productName: "Regular white T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+    },
+    {
+        productName: "Regular white T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+    },
+    {
+        productName: "Regular white T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+    },
+    {
+        productName: "Regular white T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+    }]
+}
